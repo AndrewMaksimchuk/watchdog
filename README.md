a8c617cbd6c02387060ed434011d5d96e372a400
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2020-06-07 17:47:21 +0300

    add notification, color in terminal and function to create folders

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-Add:
-- node-notifier
-- cli-color
+Make :
+
+- ~~Create "targetFolder" from config file, if not exist~~
+- ~~Create all folders for each types of saved documents, if not exist~~
+
+Add modules :
+
+- ~~node-notifier~~
+- ~~cli-color~~
--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const fs = require('fs');
 const path = require('path');
-const { exec } = require('child_process');
-const config = require('./config');
+const {
+    exec
+} = require('child_process');
+const {
+    config,
+    targetFolder
+} = require('./config');
+const notifier = require('node-notifier');
+const clc = require("cli-color");
 
 const watchFolder = config.watchFolder;
 
-fs.watch(watchFolder, '', cb);
+createTargetFolder();
 
-outputInfoText(`Start watch for "${watchFolder}".`);
+function startWatch() {
+    fs.watch(watchFolder, '', cb);
+}
 
-function cb (eventType, filename)  {
+function startMessage() {
+    console.log(clc.white.bgRed(` Start watch for "${watchFolder}" `));
+    notification('Старт', `Слідкую за змінами у папкі: ${watchFolder}.`);
+}
+
+function cb(eventType, filename) {
     if (eventType === 'change') {
         fs.readdir(watchFolder, (err, files) => {
             for (let file of files) {
                 const fileExtension = getFileType(file);
                 if (fileExtension) {
-                    const targetFolder = road(fileExtension);
-                    if (targetFolder) {
-                        move(file, targetFolder);
+                    const saveFolder = road(fileExtension);
+                    if (saveFolder) {
+                        move(file, saveFolder);
                     }
                 }
             }
@@ -25,21 +39,21 @@ function cb (eventType, filename)  {
     }
 }
 
-function getFileType (file) {
+function getFileType(file) {
     return path.extname(file);
 }
 
-function move (what, where) {
+function move(what, where) {
     exec(`move "${path.join(watchFolder, what)}" "${where}"`, () => {
-        outputInfoText(`${what}`);
+        outputInfoText(what);
     });
 }
 
-function outputInfoText (text) {
-    console.log(text);
+function outputInfoText(text) {
+    console.log(clc.green('✓ ') + text);
 }
 
-function road (ext) {
+function road(ext) {
     switch (ext) {
         case '.djvu':
             return config.saveTo.djvu;
@@ -54,4 +68,40 @@ function road (ext) {
         default:
             return config.saveTo.others;
     }
+}
+
+function notification(title, message) {
+    notifier.notify({
+        title,
+        message
+    });
+}
+
+function createTargetFolder() {
+    if (fs.existsSync(targetFolder)) {
+        createTypeFolders();
+    } else {
+        fs.mkdir(targetFolder, err => {
+            if (err) throw err;
+            createTypeFolders();
+        });
+    }
+}
+
+function createTypeFolders() {
+    const objectOfFolders = config.saveTo;
+    for (let prop in objectOfFolders) {
+        if (fs.existsSync(objectOfFolders[prop])) {
+            continue;
+        }
+        fs.mkdir(objectOfFolders[prop], err => {
+            if (err) throw err;
+        });
+    }
+    startApp();
+}
+
+function startApp() {
+    startMessage();
+    startWatch();
 }
